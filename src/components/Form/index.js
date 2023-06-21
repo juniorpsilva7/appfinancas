384003611740f3802ff6c81fce4ede968aaa8d35
Junior <juniorpsilva7@gmail.com>
2023-06-20 22:30:04 -0300

    Grid done - delete button and transactions list

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import * as C from './styles';
+import Grid from '../Grid';
 
-const Form = ({ handleAdd }) => {
+const Form = ({ handleAdd, transactionsList,setTransactionsList }) => {
   const [desc, setDesc] = useState("");
   const [amount, setAmount] = useState("");
   const [isExpense, setExpense] = useState(false);
@@ -64,6 +65,7 @@ const Form = ({ handleAdd }) => {
         </C.RadioGroup>
         <C.Button onClick={handleSave}>ADICIONAR</C.Button>
       </C.Container>
+      <Grid items={transactionsList} setItems={setTransactionsList} />
     </>
   );
 }
---
+++ src/components/Grid/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import GridItem from "../GridItem";
+import * as C from "./styles";
+
+const Grid = ({ items, setitems }) => {
+  const onDelete = (ID) => {
+    const newArray = items.filter((transaction) => transaction.id !== ID);
+    setitems(newArray);
+    localStorage.setItem("transactions", JSON.stringify(newArray));
+  };
+
+  return (
+    <C.Table>
+      <C.Thead>
+        <C.Tr>
+          <C.Th width={40}>Descrição</C.Th>
+          <C.Th width={40}>Valor</C.Th>
+          <C.Th width={10} alignCenter>
+            Tipo
+          </C.Th>
+          <C.Th width={10}></C.Th>
+        </C.Tr>
+      </C.Thead>
+      <C.Tbody>
+        {items?.map((item, index) => (
+          <GridItem key={index} item={item} onDelete={onDelete} />
+        ))}
+      </C.Tbody>
+    </C.Table>
+  );
+};
+
+export default Grid;
